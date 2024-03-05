bbac2b08193d775db615ba253308c385ee301645
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2024-03-05 16:08:31 -0300

    feat: webpage first version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="px-[5vw]">
+        <div className="max-w-4xl mx-auto">
+          <section className="pt-[8vh]">
+            <h1 className="text-4xl text-white font-semibold leading-none">
+              Olá, eu sou Rodrigo Cortezi 👋
+            </h1>
+            <p className="pt-12 leading-relaxed">
+              <span className="text-white">Eu sou Engenheiro de Software</span>{' '}
+              com uma verdadeira paixão por desenvolver soluções completas,
+              combinando o desenvolvimento Full-Stack e uma visão empreendedora
+              para trazer à vida inovações significativas. Nos útlimos 5 anos,
+              estive à frente de iniciativas marcantes, incluindo a expansão do
+              catálogo de produtos na Hurb e o lançamento bem-sucedido do
+              PartiuIngresso, um <i>marketplace</i> de ingressos.
+            </p>
+            <p className="pt-4 leading-relaxed">
+              Estou sempre em busca de desafios que me permitam explorar novas
+              tecnologias e contribuir para o desenvolvimento de produtos que
+              façam a diferença.
+            </p>
+          </section>
+          <section className="pt-11">
+            <ul className="space-y-6 leading-relaxed">
+              <li>
+                <h3 className="inline text-white">
+                  <b>Hurb</b> (2022-2023) ––&ensp;
+                </h3>
+                <p className="inline">
+                  Contribuí para o desenvolvimento de uma integração de software
+                  chave na Hurb, expandindo efetivamente o catálogo de produtos
+                  de atividades em mais de 200%. Esse aumento significativo nas
+                  ofertas melhorou substancialmente a escolha do usuário e a
+                  escalabilidade do negócio.
+                </p>
+              </li>
+              <li>
+                <h3 className="inline text-white">
+                  <b>Clarific</b> (2020-2022) ––&ensp;
+                </h3>
+                <p className="inline">
+                  Prestei consultoria em soluções de software para pequenas
+                  empresas, levando ao desenvolvimento de mais de 4 produtos de
+                  software. Esse papel envolveu compreender as necessidades
+                  únicas dos negócios, projetar soluções sob medida e
+                  implementar com sucesso essas tecnologias para impulsionar o
+                  crescimento e a eficiência.
+                </p>
+              </li>
+              <li>
+                <h3 className="inline text-white">
+                  <b>Partiu Ingresso</b> (2019-2020) ––&ensp;
+                </h3>
+                <p className="inline">
+                  Desenvolvi de forma independente a plataforma PartiuIngresso,
+                  um <i>marketplace</i> de ingressos dinâmico que alcançou mais
+                  de R$1 milhão em vendas de ingressos e atendeu mais de 8.000
+                  clientes, possibilitando um acesso adequado a shows e eventos.
+                </p>
+              </li>
+            </ul>
+          </section>
+          <section className="pt-16">
+            <p className="leading-relaxed">
+              Você pode dar uma olhada em alguns dos meus projetos no{' '}
+              <a href="https://github.com/rodrigovcortezi">GitHub</a>, ou me
+              chamar no{' '}
+              <a href="https://linkedin.com/in/rodrigo-cortezi">LinkedIn</a>. Ou
+              se preferir, pode me mandar um{' '}
+              <a href="mailto:rodrigovcortezi@gmail.com">email</a>.
+            </p>
+          </section>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </main>
+      <footer></footer>
+    </>
+  )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        base: '16px',
+        lg: '19.2px',
+        xl: '23.04px',
+        '2xl': '27.65px',
+        '3xl': '33.18px',
+        '4xl': '39.81px',
+        '5xl': '47.78px',
+      },
+      colors: {
+        gray: 'rgba(255, 255, 255, .6)',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
