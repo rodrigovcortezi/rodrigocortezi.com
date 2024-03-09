9fa9ba3308687c5dbb008146d38484ef4ddab944
Rodrigo V. Cortezi <rodrigovcortezi@gmail.com>
2024-03-09 12:21:04 -0300

    refactor(i18n): create config var for default locale

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,6 +3,7 @@ import { getRequestConfig } from 'next-intl/server'
 import { createSharedPathnamesNavigation } from 'next-intl/navigation'
 
 export const locales = ['en', 'pt-BR'] as const
+export const defaultLocale = 'pt-BR'
 export const localePrefix = 'as-needed'
 
 export const { Link } = createSharedPathnamesNavigation({
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import createMiddleware from 'next-intl/middleware'
 import type { NextRequest } from 'next/server'
-import { locales, localePrefix } from './i18n'
+import { locales, defaultLocale, localePrefix } from './i18n'
 
 const COOKIE_LOCALE_NAME = 'NEXT_LOCALE'
 
@@ -20,8 +20,8 @@ const customLocaleDetection = (request: NextRequest) => {
 
 export default async function middleware(request: NextRequest) {
   const handleI18nRouting = createMiddleware({
-    locales: ['en', 'pt-BR'],
-    defaultLocale: 'pt-BR',
+    locales,
+    defaultLocale,
     localePrefix,
   })
 
